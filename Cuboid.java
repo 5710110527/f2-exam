f2b4a06da1c98bc2991a21fb5e6a9de2a9304c99
5710110527 <5710110527@psu.ac.th>
2020-05-09 13:15:42 +0700

    Update Cuboid.java

--- Cuboid.java
+++ Cuboid.java
@@ -1,9 +1,58 @@
 public class Cuboid extends Shape implements AreaCalculable, VolumeCalculable {
-    private double radius;
+    private double width;
     private double height;
+    private double length;
 
-    public Cuboid(String name, double radius, double height){
-        super(name);
-        
+    public Cuboid(String name, double width, double height, double length){
+		super(name);
+		this.width = width;
+        this.height = height;
+        this.length = length;
     }
+    
+    public double getWidth(){
+		return width;
+	}
+
+	public void setWidth(double width){
+		this.width = width;
+	}
+
+	public double getHeight(){
+		return height;
+	}
+
+	public void setHeight(double height){
+		this.height = height;
+	}
+
+    public double getlength(){
+		return length;
+	}
+
+	public void setlength(double length){
+		this.length = length;
+    }
+    
+	public double getArea(){
+		return width * height;
+    }
+    
+    public double getVolume(){
+		return width * height *length;
+	}
+
+	public void zoom(double factor){
+		width = width * factor;
+        height = height * factor;
+        length = length * factor;
+    }
+    
+    public static void main(String[] args){
+        Rectangle c = new Rectangle("R1", 2.0, 8.0);
+        Cuboid er = new Cuboid("R2",2.0,8.0,3.0)
+        System.out.println("Area of " + c.getName() + " is " + c.getArea());
+        System.out.println("Area of " + er.getName() + " is " + c.getVolume());
+	}
+
 }
