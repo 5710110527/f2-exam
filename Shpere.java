bc2f969b7fb900221298fa10b1aeeafb0a7a1d09
5710110527 <5710110527@psu.ac.th>
2020-05-09 13:40:12 +0700

    Update Shpere.java

--- Shpere.java
+++ Shpere.java
@@ -3,4 +3,16 @@ public class Shpere extends Shape implements AreaCalculable, VolumeCalculable{
     public Shpere(){
         
     }
+
+    public double getArea(){
+        return 4*Math.PI*radius*radius;
+    }
+
+   public double getVolume(){
+       return (4/3)*Math.PI*radius*radius*radius;
+   }
+   
+   public void zoom(double factor){
+       radius = radius*factor;
+   }
 }
