4a06694a252e17124ec0cece5898973275e6b6f7
5710110527 <5710110527@psu.ac.th>
2020-05-09 11:27:18 +0700

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,3 +1,11 @@
 public abstract class Shape {
-    public Shape 
+    public String name;
+
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        retune name;
+    }
 }
