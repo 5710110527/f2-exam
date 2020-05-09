139cf412c10166ffd747c85753d31c0befb459aa
5710110527 <5710110527@psu.ac.th>
2020-05-09 12:58:34 +0700

    Update Cuboid.java

---
+++ Cuboid.java
@@ -0,0 +1,9 @@
+public class Cuboid extends Shape implements AreaCalculable, VolumeCalculable {
+    private double radius;
+    private double height;
+
+    public Cuboid(String name, double radius, double height){
+        super(name);
+        
+    }
+}
