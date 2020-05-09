87875f5f1a114177f79b56e34be3e20f29b41114
5710110527 <5710110527@psu.ac.th>
2020-05-09 13:52:10 +0700

    Update Cuboid.java

--- Cuboid.java
+++ Cuboid.java
@@ -49,10 +49,9 @@ public class Cuboid extends Shape implements AreaCalculable, VolumeCalculable {
     }
     
     public static void main(String[] args){
-        Rectangle c = new Rectangle("R1", 2.0, 8.0);
         Cuboid er = new Cuboid("R2",2.0,8.0,3.0);
-        System.out.println("Area of " + c.getName() + " is " + c.getArea());
-        System.out.println("Area of " + er.getName() + " is " + er.getVolume());
-	}
+        System.out.println("Area of " + er.getName() + " is " + er.getArea());
+        System.out.println("Volume of " + er.getName() + " is " + er.getVolume());
+    }
 
 }
