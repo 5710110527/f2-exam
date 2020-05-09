348d26aab2dec463e64badab4bb111621d099516
5710110527 <5710110527@psu.ac.th>
2020-05-09 13:26:54 +0700

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,29 +1,9 @@
-import java.util.Scanner;
-
 public class Circle extends Shape implements AreaCalculable {
     private double radius;
 
     public Circle(String name, double radius){
 		super(name);
 		this.radius = radius;
-    }
-
-    public double spliteString(String key){
-        try {
-        int r = 0;
-        Scanner s = new Scanner(new File("data,txt"));
-        while(s.hasNextLine()){
-            String line = s.nextLine();
-            String item = line.splite("_");
-            if(item[0].equals(key)){
-                r = Integer.pasrseInt(item[1]);
-            }
-        }
-        s.close();
-        }catch (FileNotFoundException e) {
-         System.out.println("An error occurred.");
-         e.printStackTrace();
-        }
     }
     
     public double getArea(){
