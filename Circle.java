a4a6c829ab2a10885c8d123e155cc74eb935b34f
5710110527 <5710110527@psu.ac.th>
2020-05-09 11:27:16 +0700

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,3 +1,13 @@
-public class Circle implements AreaCalculable {
-    private double Re
+public class Circle extends Shape implements AreaCalculable {
+    private double radius;
+
+    public Circle(String name, double radius){
+		super(name);
+		this.radius = radius;
+    }
+    
+    public double getArea(){
+		return Math.PI * radius * radius;
+    }
+    
 }
